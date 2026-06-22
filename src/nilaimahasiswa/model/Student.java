29f94b80c16119ae68dc45f0a44f097650b1e1c4
WanXdOfc <iketutdharmawan2007@gmail.com>
2026-06-22 20:35:52 +0800

    Change Input password from textField to PasswordField.

--- src/nilaimahasiswa/model/Student.java
+++ src/nilaimahasiswa/model/Student.java
@@ -15,12 +15,22 @@ import java.util.ArrayList;
 public class Student extends Person {
     private String nim;
     private String studyProgram;
+    private String angkatan;
     private ArrayList<KRS> krsList;
-
+    
     public Student(String idCard, String name, String nim, String studyProgram) {
         super(idCard, name);
         this.nim = nim;
         this.studyProgram = studyProgram;
+        this.angkatan = "";
+        krsList = new ArrayList<>();
+    }
+    
+    public Student(String idCard, String name, String nim, String studyProgram, String angkatan) {
+        super(idCard, name);
+        this.nim = nim;
+        this.studyProgram = studyProgram;
+        this.angkatan = angkatan;
         krsList = new ArrayList<>();
     }
 
--- src/nilaimahasiswa/view/LoginForm.java
+++ src/nilaimahasiswa/view/LoginForm.java
@@ -37,7 +37,22 @@ public class LoginForm extends javax.swing.JFrame {
 
         // Input placeholder (kalau belum ada)
         jTextFieldUsername.putClientProperty("JTextField.placeholderText", "Masukkan username");
-        jTextFieldPassword.putClientProperty("JTextField.placeholderText", "Masukkan password");
+        jPasswordField.putClientProperty("JTextField.placeholderText", "Masukkan password");
+        
+        jCheckBoxShowPassword.setFont(new java.awt.Font("Segoe UI", java.awt.Font.PLAIN, 12));
+        
+        jCheckBoxShowPassword.addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                if (jCheckBoxShowPassword.isSelected()) {
+                    // Mengubah echo char ke 0 artinya teks password akan terlihat jelas (di-unmask)
+                    jPasswordField.setEchoChar((char) 0);
+                } else {
+                    // Mengembalikan tampilan menjadi bulat/bintang tersembunyi (default Swing)
+                    jPasswordField.setEchoChar('\u2022'); 
+                }
+            }
+        });
     }
 
     /**
@@ -48,7 +63,6 @@ public class LoginForm extends javax.swing.JFrame {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
-        java.awt.GridBagConstraints gridBagConstraints;
 
         jPanel1 = new javax.swing.JPanel();
         jPanel2 = new javax.swing.JPanel();
@@ -57,9 +71,10 @@ public class LoginForm extends javax.swing.JFrame {
         jPanel4 = new javax.swing.JPanel();
         jLabel3 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
-        jTextFieldPassword = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
         jButtonLogin = new javax.swing.JButton();
+        jPasswordField = new javax.swing.JPasswordField();
+        jCheckBoxShowPassword = new javax.swing.JCheckBox();
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -82,7 +97,7 @@ public class LoginForm extends javax.swing.JFrame {
         jPanel3.setMinimumSize(new java.awt.Dimension(400, 500));
         jPanel3.setPreferredSize(new java.awt.Dimension(400, 500));
 
-        jTextFieldUsername.setHorizontalAlignment(javax.swing.JTextField.CENTER);
+        jTextFieldUsername.setHorizontalAlignment(javax.swing.JTextField.LEFT);
         jTextFieldUsername.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 3, true));
         jTextFieldUsername.addActionListener(this::jTextFieldUsernameActionPerformed);
 
@@ -114,10 +129,6 @@ public class LoginForm extends javax.swing.JFrame {
         jLabel1.setFont(new java.awt.Font("Sitka Text", 1, 18)); // NOI18N
         jLabel1.setText("Username");
 
-        jTextFieldPassword.setHorizontalAlignment(javax.swing.JTextField.CENTER);
-        jTextFieldPassword.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 3, true));
-        jTextFieldPassword.addActionListener(this::jTextFieldPasswordActionPerformed);
-
         jLabel2.setFont(new java.awt.Font("Sitka Text", 1, 18)); // NOI18N
         jLabel2.setText("Password");
 
@@ -127,27 +138,36 @@ public class LoginForm extends javax.swing.JFrame {
         jButtonLogin.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         jButtonLogin.addActionListener(this::jButtonLoginActionPerformed);
 
+        jPasswordField.setBorder(new javax.swing.border.LineBorder(new java.awt.Color(0, 0, 0), 3, true));
+        jPasswordField.setOpaque(true);
+        jPasswordField.addActionListener(this::jPasswordFieldActionPerformed);
+
+        jCheckBoxShowPassword.setText("Show Password");
+        jCheckBoxShowPassword.addActionListener(this::jCheckBoxShowPasswordActionPerformed);
+
         javax.swing.GroupLayout jPanel3Layout = new javax.swing.GroupLayout(jPanel3);
         jPanel3.setLayout(jPanel3Layout);
         jPanel3Layout.setHorizontalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                .addGap(0, 74, Short.MAX_VALUE)
-                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
-                    .addComponent(jTextFieldUsername, javax.swing.GroupLayout.PREFERRED_SIZE, 260, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                            .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGap(151, 151, 151))
-                        .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
-                            .addComponent(jTextFieldPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 260, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addGap(66, 66, 66)))
-                    .addComponent(jLabel2)))
             .addGroup(jPanel3Layout.createSequentialGroup()
-                .addGap(105, 105, 105)
-                .addComponent(jButtonLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(74, 74, 74)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jCheckBoxShowPassword)
+                    .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(jPasswordField, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jLabel1, javax.swing.GroupLayout.Alignment.LEADING)
+                        .addComponent(jTextFieldUsername, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 260, Short.MAX_VALUE)
+                        .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.LEADING)))
+                .addGap(0, 0, Short.MAX_VALUE))
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                .addContainerGap(110, Short.MAX_VALUE)
+                .addGroup(jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                        .addComponent(jPanel4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(151, 151, 151))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel3Layout.createSequentialGroup()
+                        .addComponent(jButtonLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 191, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(99, 99, 99))))
         );
         jPanel3Layout.setVerticalGroup(
             jPanel3Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -161,10 +181,12 @@ public class LoginForm extends javax.swing.JFrame {
                 .addGap(28, 28, 28)
                 .addComponent(jLabel2)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jTextFieldPassword, javax.swing.GroupLayout.PREFERRED_SIZE, 47, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(43, 43, 43)
-                .addComponent(jButtonLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 31, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(113, Short.MAX_VALUE))
+                .addComponent(jPasswordField, javax.swing.GroupLayout.PREFERRED_SIZE, 46, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jCheckBoxShowPassword)
+                .addGap(27, 27, 27)
+                .addComponent(jButtonLogin, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(96, Short.MAX_VALUE))
         );
 
         jPanel2.add(jPanel3, new java.awt.GridBagConstraints());
@@ -178,13 +200,9 @@ public class LoginForm extends javax.swing.JFrame {
         // TODO add your handling code here:
     }//GEN-LAST:event_jTextFieldUsernameActionPerformed
 
-    private void jTextFieldPasswordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jTextFieldPasswordActionPerformed
-        // TODO add your handling code here:
-    }//GEN-LAST:event_jTextFieldPasswordActionPerformed
-
     private void jButtonLoginActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButtonLoginActionPerformed
         String username = jTextFieldUsername.getText().trim();
-        String password = jTextFieldPassword.getText().trim();
+        String password = jPasswordField.getText().trim();
 
         // Validasi input kosong
         if (username.isEmpty() || password.isEmpty()) {
@@ -217,6 +235,20 @@ public class LoginForm extends javax.swing.JFrame {
         }
     }//GEN-LAST:event_jButtonLoginActionPerformed
 
+    private void jPasswordFieldActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jPasswordFieldActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jPasswordFieldActionPerformed
+
+    private void jCheckBoxShowPasswordActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jCheckBoxShowPasswordActionPerformed
+         if (jCheckBoxShowPassword.isSelected()) {
+            // Angka 0 artinya karakter penutup dihapus (password terlihat asli)
+            jPasswordField.setEchoChar((char) 0); 
+        } else {
+            // Mengembalikan tampilan menjadi titik bulat hitam saat centang dilepas
+            jPasswordField.setEchoChar('\u2022'); 
+        }
+    }//GEN-LAST:event_jCheckBoxShowPasswordActionPerformed
+
     /**
      * @param args the command line arguments
      */
@@ -237,6 +269,7 @@ public class LoginForm extends javax.swing.JFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton jButtonLogin;
+    private javax.swing.JCheckBox jCheckBoxShowPassword;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
@@ -244,7 +277,7 @@ public class LoginForm extends javax.swing.JFrame {
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JPanel jPanel4;
-    private javax.swing.JTextField jTextFieldPassword;
+    private javax.swing.JPasswordField jPasswordField;
     private javax.swing.JTextField jTextFieldUsername;
     // End of variables declaration//GEN-END:variables
 }
